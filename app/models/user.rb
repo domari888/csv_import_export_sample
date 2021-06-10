bd9f78d1c621aeceb57b2ded7dc41500d5f4b98d
damari <motokin128@gmail.com>
2021-06-10 23:48:15 +0900

    insert_all を用いて CSV インポートを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   CSV_COLUMNS = %w[name age height].freeze
   
   def self.import_csv(file:)
+    now = Time.current
+    timestamp_attributes = {created_at: now, updated_at: now}
     list = []
     CSV.foreach(file.path, headers: true) do |row|
         # インポートで受け取れるカラムを制限(余計なものがあった際はエラー)
         # `*` は配列を展開する
-        list << row.to_h.slice(*CSV_COLUMNS)
+        list << row.to_h.slice(*CSV_COLUMNS).merge(timestamp_attributes)
     end
       # User モデルにバルクインサート(まとめて追加)
-    User.import!(list)
+    User.insert_all(list)
   end
 end
